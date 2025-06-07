cf8e8432a9b692e23d9110ee9710678dd4dbd424
nixonmkindi <nixonrichardpc0@gmail.com>
2025-06-07 11:25:07 +0300

    _

--- package/api/nacte/nacte.go
+++ package/api/nacte/nacte.go
@@ -1,8 +1,1 @@
 package nacte
-
-func GetPaymentAPIKey(paymentRef string) string {
-	if apiKey, exists := paymentAPIKeys[paymentRef]; exists {
-		return apiKey
-	}
-	return ""
-}
--- package/api/nacte/nacte_config.go
+++ package/api/nacte/nacte_config.go
@@ -1,15 +1,1 @@
 package nacte
-
-const (
-	mainCampusPaymentAPIKey = "hife2925ab594b56.b5fdf320a3961b950b85b729e2657dadddd02a79e9bef7dd739917949ba24926.d96a0b4a5fca45a7f98819beaa121bc0bc7bd07e"
-
-	mwanzaCampusPaymentAPIKey = "jk1a1652e67aefff.0c5a803893be597c5e89c465f0e87dfbf32b89573b16135e7b2b8908105b4dd2.960b4bdac1744048c32625a5b3167cdaeb2a969a"
-
-	myungaCampusPaymentAPIKey = "PQ74b818281c6ff7.c3a7b66e1a9ecfd7396acd42da7d7490db82f070f2b22f488b68a8dc764d36c6.94b1b263d66f3a2e5bf6c48c111f10c7c223dcee"
-)
-
-var paymentAPIKeys = map[string]string{
-	"924213266982249": mainCampusPaymentAPIKey,
-	"924216267702099": mwanzaCampusPaymentAPIKey,
-	"924220268639869": myungaCampusPaymentAPIKey,
-}
